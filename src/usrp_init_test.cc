2b1d731f65e2eaa70e2a5fce7687c56a23a9904e
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-26 21:40:15 +0000

    connecting to usrp: printing output

--- src/usrp_init_test.cc
+++ src/usrp_init_test.cc
@@ -24,6 +24,29 @@ int main() {
     std::cout << "rx db slot 0 : " << usrp_dbid_to_string(rx_db0) << std::endl;
     std::cout << "rx db slot 1 : " << usrp_dbid_to_string(rx_db1) << std::endl;
 
+    if (rx_db0 == USRP_DBID_FLEX_400_RX_MIMO_B)
+        printf("ok!!!\n");
+
+    unsigned int buffer_len = 1024;
+    short int rx_buffer[buffer_len];
+    bool overrun=false;
+
+    // start
+    usrp_rx->start();
+
+    unsigned int i, j;
+    for (i=0; i<10; i++) {
+        usrp_rx->read(rx_buffer, buffer_len, &overrun);
+
+        // print several values of buffer
+        printf("%4u: ", i);
+        for (j=0; j<8; j++)
+            printf("%8d, ", rx_buffer[j]);
+        printf("\n");
+    }
+
+    usrp_rx->stop();
+
     printf("done.\n");
     return 0;
 }
