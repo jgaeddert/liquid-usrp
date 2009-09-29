1629809f443f84ab185da43915f52188f7eceff9
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-09-29 12:59:38 +0000

    tx_rrc gets a makeover

--- src/tx_rrc.cc
+++ src/tx_rrc.cc
@@ -29,6 +29,7 @@
 #include <math.h>
 #include <iostream>
 #include <complex>
+#include <getopt.h>
 #include <liquid/liquid.h>
 
 #include "usrp_standard.h"
@@ -36,6 +37,7 @@
 #include "usrp_dbid.h"
 #include "usrp_bytesex.h"
 #include "flex.h"
+#include "usrp_io.h"
  
 /*
  SAMPLES_PER_READ :Each sample is consists of 4 bytes (2 bytes for I and 
@@ -45,214 +47,154 @@
  */
 #define SAMPLES_PER_READ    (512)       // Must be a multiple of 128
 #define USRP_CHANNEL        (0)
- 
-// Dummy Function to process USRP data
-void process_data(int *buffer)
-{
-/*
-Each buffer element, for example buffer[0] contains 4 bytes 
-2 bytes For (I) and 2 bytes for (Q). 
-*/
- 
-    unsigned int i;
-    short I,Q;
-    float e=0.0f;
-    for (i=0; i<SAMPLES_PER_READ; i++) {
-        I =  buffer[i] & 0x0000ffff;
-        Q = (buffer[i] & 0xffff0000) >> 16;
-        //printf("(%5d+j%5d), ", I, Q);
-        e += fabsf(I) + fabsf(Q);
-    }
-    //printf("\n");
-    e /= SAMPLES_PER_READ;
-    printf("e: %8.4f\n", e);
 
+void usage() {
+    printf("packet_tx:\n");
+    printf("  f     :   center frequency [Hz]\n");
+    printf("  s     :   symbol rate [Hz] (62.5kHz min, 8MHz max)\n");
+    printf("  t     :   run time [seconds]\n");
+    printf("  m     :   filter delay [symbols]\n");
+    printf("  b     :   filter excess bandwidth factor [0.0 min, 1.0 max]\n");
+    printf("  q     :   quiet\n");
+    printf("  v     :   verbose\n");
+    printf("  u,h   :   usage/help\n");
 }
- 
+
 int main (int argc, char **argv)
 {
-    //bool   loopback_p = false;
-    //bool   counting_p = false;
-    //bool   width_8_p = false;
-    //double center_freq = 0;
-    int    nchannels = 1;
-    int    nunderruns = 0;
-    bool   underrun;
-    int    total_writes = 100000;
-    int    i;
-    //int    buf[SAMPLES_PER_READ];
-    //int    bufsize = SAMPLES_PER_READ*4; // Should be multiple of 512 Bytes
-    const int tx_buf_len = 512;
-    short tx_buf[tx_buf_len];
+    // command-line options
+    bool verbose = true;
 
-#if 0 
-    if (loopback_p)    mode |= usrp_standard_tx::FPGA_MODE_LOOPBACK;
-   
-    if (counting_p)    mode |= usrp_standard_tx::FPGA_MODE_COUNTING;
-#endif
- 
-#if 1
-    int    which_board = 0;
-    //int    decim = 256;            // 8 -> 32 MB/sec
-    int    interp_rate = 512;
-    int    fusb_block_size = 0;
-    int    fusb_nblocks = 0;
-    //int    gain = 0;
-    int    mode = 0;
-    usrp_standard_tx *utx = 
-        usrp_standard_tx::make (which_board, interp_rate, 1, -1, //mode,
-                fusb_block_size, fusb_nblocks);
-#else
-    usrp_standard_tx *utx = usrp_standard_tx::make(0, 512);
-#endif
- 
-    if (utx == 0) {
-        fprintf (stderr, "Error: usrp_standard_tx::make\n");
-        exit (1);
+    float min_symbol_rate = (32e6 / 512.0);
+    float max_symbol_rate = (32e6 /   4.0);
+
+    float frequency = 462.0e6;
+    float symbol_rate = min_symbol_rate;
+    float num_seconds = 5.0f;
+    unsigned int m = 3;
+    float beta = 0.3f;
+
+
+    //
+    int d;
+    while ((d = getopt(argc,argv,"f:s:t:m:b:qvuh")) != EOF) {
+        switch (d) {
+        case 'f':   frequency = atof(optarg);       break;
+        case 's':   symbol_rate = atof(optarg);     break;
+        case 't':   num_seconds = atof(optarg);     break;
+        case 'm':   m = atoi(optarg);               break;
+        case 'b':   beta = atof(optarg);            break;
+        case 'q':   verbose = false;                break;
+        case 'v':   verbose = true;                 break;
+        case 'u':
+        case 'h':
+        default:
+            usage();
+            return 0;
+        }
     }
 
-#if 0
-    if (width_8_p) {
-        int width = 8;
-        int shift = 8;
-        bool want_q = true;
-        if (!utx->set_format(usrp_standard_tx::make_format(width, shift, want_q))) {
-            fprintf (stderr, "Error: utx->set_format\n");
-            exit (1);
-       }
-    }
-#endif
+    // compute interpolation rate
+    unsigned int interp_rate = (unsigned int)(32e6 / symbol_rate);
+    
+    // ensure multiple of 4
+    interp_rate = (interp_rate >> 2) << 2;
 
-    // daughterboard
-    int tx_db0 = utx->daughterboard_id(0);
-    db_base * tx_db0_control;   // from ossie
-    std::cout << "tx db slot 0 : " << usrp_dbid_to_string(tx_db0) << std::endl;
- 
-    // Set DDC center frequency
-    //utx->set_tx_freq (0, center_freq);
- 
-     // Set Number of channels
-    utx->set_nchannels(nchannels);
- 
-    // Set ADC PGA gain
-    //utx->set_pga(0,gain);
- 
-    // Set FPGA Mux
-    //utx->set_mux(0x32103210); // Board A only
- 
-    // Set DDC decimation rate
-    //utx->set_decim_rate(decim);
-  
-    // Set DDC phase 
-    //utx->set_ddc_phase(0,0);
+    // update actual symbol_rate
+    symbol_rate = 32e6f / (float)(interp_rate);
 
-
-    if (tx_db0 == USRP_DBID_FLEX_400_TX_MIMO_B) {
-        printf("usrp daughterboard: USRP_DBID_FLEX_400_TX_MIMO_B\n");
-        tx_db0_control = new db_flex400_tx_mimo_b(utx,0);
-    } else {
-        printf("use usrp db flex 400 tx MIMO B\n");
+    if (symbol_rate > max_symbol_rate) {
+        printf("error: maximum symbol_rate exceeded (%8.4f MHz)\n", max_symbol_rate*1e-6);
         return 0;
-    }   
+    } else if (symbol_rate < min_symbol_rate) {
+        printf("error: minimum symbol_rate exceeded (%8.4f kHz)\n", min_symbol_rate*1e-3);
+        return 0;
+    } else if (m < 1 || m > 20) {
+        printf("error: filter length m must be in [1,20]\n");
+        return 0;
+    } else if (beta < 0.0f || beta > 1.0f) {
+        printf("error: filter excess bandwidth beta must be in [0.0,1.0]\n");
+        return 0;
+    }
 
-    // set the ddc frequency
-    utx->set_tx_freq(USRP_CHANNEL, 0.0);
+    printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
+    printf("symbol_rate :   %12.8f [kHz]\n", symbol_rate*1e-3f);
+    printf("verbosity   :   %s\n", (verbose?"enabled":"disabled"));
 
-    // set the daughterboard gain
-    float gmin, gmax, gstep;
-    tx_db0_control->get_gain_range(gmin,gmax,gstep);
-    printf("gmin/gmax/gstep: %f/%f/%f\n", gmin,gmax,gstep);
-    tx_db0_control->set_gain(gmax);
+    // create usrp_io object and set properties
+    usrp_io * uio = new usrp_io();
+    uio->set_tx_freq(0, frequency);
+    uio->set_tx_interp(interp_rate);
+    uio->enable_auto_tx(0);
 
-    // set the daughterboard frequency
-    float fmin, fmax, fstep;
-    tx_db0_control->get_freq_range(fmin,fmax,fstep);
-    printf("fmin/fmax/fstep: %f/%f/%f\n", fmin,fmax,fstep);
-    float frequency = 462e6;
-    float db_lo_offset = -8e6;
-    float db_lo_freq = 0.0f;
-    float db_lo_freq_set = frequency + db_lo_offset;
-    tx_db0_control->set_db_freq(db_lo_freq_set, db_lo_freq);
-    printf("lo frequency: %f MHz (actual: %f MHz)\n", db_lo_freq_set/1e6, db_lo_freq/1e6);
-    float ddc_freq_set = frequency - db_lo_freq;
-    utx->set_tx_freq(USRP_CHANNEL, ddc_freq_set);
-    float ddc_freq = utx->tx_freq(USRP_CHANNEL);
-    printf("ddc freq: %f MHz (actual %f MHz)\n", ddc_freq_set/1e6, ddc_freq/1e6);
+    // retrieve tx port from usrp_io object
+    gport port_tx = uio->get_tx_port(0);
 
-    // create interpolator
-    unsigned int k=4; // samples per symbol
-    unsigned int m=3; // delay
-    float beta=0.3f;  // excess bandwidth factor
+    unsigned int num_symbols = 128;
 
+    // filter parameters
+    unsigned int k=2;   // samples/symbol
+    //unsigned int m=3;   // symbol delay
+    float dt=0.0f;      // fractional sample delay
+    //float beta=0.3f;    // excess bandwidth factor
+
+    // create filter
     unsigned int h_len = 2*k*m+1;
     float h[h_len];
-    design_rrc_filter(k,m,beta,0,h);
-    h[h_len-1] = h[0];
-    interp_crcf interpolator = interp_crcf_create(k,h,h_len);
-    std::complex<float> interp_buffer[k];
-    std::complex<float> symbol;
+    design_rrc_filter(k,m,beta,dt,h);
+    interp_crcf nyquist_filter = interp_crcf_create(k,h,h_len);
 
-    modulation_scheme ms = MOD_PSK;
+    resamp2_crcf interpolator = resamp2_crcf_create(37,0.0f,60.0f);
+    std::complex<float> * data_tx;
+
+    // 
+    std::complex<float> symbols[num_symbols];
+    std::complex<float> interp_out[2*num_symbols];
+
+    // modem
+    modulation_scheme ms = MOD_QPSK;
     unsigned int bps = 2;
-    modem linmod = modem_create(ms, bps);
-    unsigned int s;
+    modem mod = modem_create(ms,bps);
 
-    // generate data buffer
-    short I, Q;
-    printf("USRP Transfer Started\n");
-    tx_db0_control->set_enable(true);
-    utx->start();        // Start data transfer
- 
-    unsigned int j, n;
-    // Do USRP Samples Reading 
+    unsigned int n; // sample counter
+    unsigned int s; // data symbol
+    
+    int    total_writes = 1000000;
+    int    i;
+
+    // start USRP data transfer
+    uio->start_tx(0);
     for (i = 0; i < total_writes; i++) {
-        // generate random data
-        for (j=0; j<tx_buf_len; j+=2*k) {
-            symbol.real() = rand()%2 ? 1.0f : -1.0f;
-            symbol.imag() = rand()%2 ? 1.0f : -1.0f;
 
-            s = modem_gen_rand_sym(linmod);
-            modem_modulate(linmod, s, &symbol);
+        // retrieve tx data buffer
+        data_tx = (std::complex<float>*) gport_producer_lock(port_tx,512);
 
-            // run interpolator
-            interp_crcf_execute(interpolator, symbol, interp_buffer);
-            for (n=0; n<k; n++) {
-                I = (short) (interp_buffer[n].real() * 1000);
-                Q = (short) (interp_buffer[n].imag() * 1000);
-
-                tx_buf[j+2*n+0] = host_to_usrp_short(I);
-                tx_buf[j+2*n+1] = host_to_usrp_short(Q);
-            }
+        // generate data symbols
+        for (n=0; n<num_symbols; n++) {
+            s = modem_gen_rand_sym(mod);
+            modem_modulate(mod,s,&symbols[n]);
         }
 
-        //utx->write(&buf, bufsize, &underrun); 
-        int rc = utx->write(tx_buf, tx_buf_len*sizeof(short), &underrun); 
-            
-        if (underrun) {
-            printf ("USRP tx underrun\n");
-            nunderruns++;
-        }
+        // run nyquist filter/interpolator
+        for (n=0; n<num_symbols; n++)
+            interp_crcf_execute(nyquist_filter,symbols[n],&interp_out[2*n]);
 
-        if (rc < 0) {
-            printf("error occurred with USRP\n");
-            exit(0);
-        } else if (rc != tx_buf_len*sizeof(short)) {
-            printf("error: did not write proper length\n");
-            exit(0);
-        }
- 
-        // Do whatever you want with the data
-        //process_data(&buf[0]);
+        // run half-band interpolator
+        for (n=0; n<2*num_symbols; n++)
+            resamp2_crcf_interp_execute(interpolator,interp_out[n],&data_tx[2*n]);
+
+        gport_producer_unlock(port_tx,512);
+
     }
  
- 
-    utx->stop();  // Stop data transfer
-    printf("USRP Transfer Stopped\n");
+    uio->stop_tx(0);  // Stop data transfer
+    printf("usrp data transfer complete\n");
 
     // clean it up
-    interp_crcf_destroy(interpolator);
-    modem_destroy(linmod);
-    delete utx;
+    resamp2_crcf_destroy(interpolator);
+    interp_crcf_destroy(nyquist_filter);
+    modem_destroy(mod);
+    delete uio;
     return 0;
 }
 
