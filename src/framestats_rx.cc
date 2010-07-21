270da4ffad8cd4f660ab1d66cd26eb77df6848e7
Joseph Gaeddert <jgaedder@vt.edu>
2010-07-21 12:44:20 -0400

    adding frame statstics tx/rx applications

---
+++ src/framestats_rx.cc
@@ -0,0 +1,360 @@
+/*
+ * Copyright (c) 2010 Joseph Gaeddert
+ * Copyright (c) 2010 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// framestats_tx.cc
+//
+// frame statistics transmitter
+//
+
+#include <iostream>
+#include <complex>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <liquid/liquid.h>
+
+#include "usrp_io.h"
+ 
+#define USRP_CHANNEL        (0)
+ 
+static bool verbose;
+
+static float SNRdB_av;
+
+unsigned int num_steps = 31;
+float SNRdB_min =  0.0f;
+float SNRdB_max = 30.0f;
+float * SNRdB;
+static unsigned int * num_packets_received;
+static unsigned int * num_valid_headers_received;
+static unsigned int * num_valid_packets_received;
+static unsigned int * num_bytes_received;
+
+typedef struct {
+    unsigned char * header;
+    unsigned char * payload;
+    packetizer p;
+} framedata;
+
+static int callback(unsigned char * _rx_header,
+                    int _rx_header_valid,
+                    unsigned char * _rx_payload,
+                    unsigned int _rx_payload_len,
+                    framesyncstats_s _stats,
+                    void * _userdata)
+{
+    // estimate SNR
+    float noise_floor = -38.0f;
+    float SNRdB_hat = _stats.rssi - noise_floor;
+
+    // compute array index
+    int i = 0;
+    float SNRdB_step = (SNRdB_max - SNRdB_min) / (float)(num_steps-1);
+    i = (int) floorf( (SNRdB_hat - SNRdB_min) / SNRdB_step );
+    if (i < 0) {
+        fprintf(stderr,"warning: SNR estimate below boundary (low)\n");
+        i = 0;
+    } else if (i > (int)num_steps-1) {
+        fprintf(stderr,"warning: SNR estimate below boundary (high)\n");
+        i = num_steps-1;
+    }
+    //printf("  SNR: %6.2f  ->  %d\n", SNRdB_hat, i);
+
+    num_packets_received[i]++;
+    if (verbose) {
+        printf("********* callback invoked, ");
+        printf("SNR=%5.1fdB, ", _stats.SNR);
+        printf("rssi=%5.1fdB, ", _stats.rssi);
+    }
+
+    // make better estimate of SNR
+    SNRdB_av += SNRdB_hat;
+
+    if ( !_rx_header_valid ) {
+        if (verbose) printf("header crc : FAIL\n");
+        return 0;
+    }
+    num_valid_headers_received[i]++;
+    unsigned int packet_id = (_rx_header[0] << 8 | _rx_header[1]);
+    if (verbose) printf("packet id: %6u\n", packet_id);
+    unsigned int payload_len = (_rx_header[2] << 8 | _rx_header[3]);
+    fec_scheme fec0 = (fec_scheme)(_rx_header[4]);
+    fec_scheme fec1 = (fec_scheme)(_rx_header[5]);
+
+    /*
+    // TODO: validate fec0,fec1 before indexing fec_scheme_str
+    printf("    payload len : %u\n", payload_len);
+    printf("    fec0        : %s\n", fec_scheme_str[fec0]);
+    printf("    fec1        : %s\n", fec_scheme_str[fec1]);
+    */
+
+    framedata * fd = (framedata*) _userdata;
+    fd->p = packetizer_recreate(fd->p, payload_len, fec0, fec1);
+
+    // decode packet
+    unsigned char msg_dec[payload_len];
+    bool crc_pass = packetizer_decode(fd->p, _rx_payload, msg_dec);
+    if (crc_pass) {
+        num_valid_packets_received[i]++;
+        num_bytes_received[i] += payload_len;
+    } else {
+        if (verbose) printf("  <<< payload crc fail >>>\n");
+    }
+
+    /*
+    packetizer_print(fd->p);
+    printf("payload len: %u\n", _rx_payload_len);
+    unsigned int i;
+    for (i=0; i<_rx_payload_len; i++)
+        printf("%.2x ", _rx_payload[i]);
+    printf("\n");
+
+    for (i=0; i<payload_len; i++)
+        printf("%.2x ", msg_dec[i]);
+    printf("\n");
+    */
+
+    return 0;
+}
+
+double calculate_execution_time(struct rusage _start, struct rusage _finish)
+{
+    double utime = _finish.ru_utime.tv_sec - _start.ru_utime.tv_sec
+        + 1e-6*(_finish.ru_utime.tv_usec - _start.ru_utime.tv_usec);
+    double stime = _finish.ru_stime.tv_sec - _start.ru_stime.tv_sec
+        + 1e-6*(_finish.ru_stime.tv_usec - _start.ru_stime.tv_usec);
+    //printf("utime : %12.8f, stime : %12.8f\n", utime, stime);
+    return utime + stime;
+}
+
+void usage() {
+    printf("packet_tx:\n");
+    printf("  f     :   center frequency [Hz]\n");
+    printf("  b     :   bandwidth [Hz]\n");
+    printf("  t     :   run time [seconds]\n");
+    printf("  o     :   output filename, default: framestats.dat\n");
+    printf("  q     :   quiet\n");
+    printf("  v     :   verbose\n");
+    printf("  u,h   :   usage/help\n");
+}
+
+int main (int argc, char **argv)
+{
+    // command-line options
+    verbose = true;
+
+    float min_bandwidth = (32e6 / 512.0);
+    float max_bandwidth = (32e6 /   4.0);
+
+    float frequency = 462.0e6;
+    float bandwidth = min_bandwidth;
+    float num_seconds = 5.0f;
+
+    char filename[256] = "framestats.dat";
+
+    //
+    int d;
+    while ((d = getopt(argc,argv,"f:b:t:o:qvuh")) != EOF) {
+        switch (d) {
+        case 'f':   frequency = atof(optarg);       break;
+        case 'b':   bandwidth = atof(optarg);       break;
+        case 't':   num_seconds = atof(optarg);     break;
+        case 'o':   strncpy(filename,optarg,256);   break;
+        case 'q':   verbose = false;                break;
+        case 'v':   verbose = true;                 break;
+        case 'u':
+        case 'h':
+        default:
+            usage();
+            return 0;
+        }
+    }
+
+    if (bandwidth > max_bandwidth) {
+        printf("error: maximum bandwidth exceeded (%8.4f MHz)\n", max_bandwidth*1e-6);
+        return 0;
+    } else if (bandwidth < min_bandwidth) {
+        printf("error: minimum bandwidth exceeded (%8.4f kHz)\n", min_bandwidth*1e-3);
+        return 0;
+    }
+
+    printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
+    printf("bandwidth   :   %12.8f [kHz]\n", bandwidth*1e-3f);
+    printf("verbosity   :   %s\n", (verbose?"enabled":"disabled"));
+
+    // initialize arrays
+    SNRdB = (float *) malloc(num_steps * sizeof(float));
+    num_packets_received        = (unsigned int *) malloc(num_steps * sizeof(unsigned int));
+    num_valid_headers_received  = (unsigned int *) malloc(num_steps * sizeof(unsigned int));
+    num_valid_packets_received  = (unsigned int *) malloc(num_steps * sizeof(unsigned int));
+    num_bytes_received          = (unsigned int *) malloc(num_steps * sizeof(unsigned int));
+
+    unsigned int i;
+    float SNRdB_step = (SNRdB_max - SNRdB_min) / (float)(num_steps - 1);
+    for (i=0; i<num_steps; i++) {
+        SNRdB[i] = SNRdB_min + i*SNRdB_step;
+        //printf("    %3u : %6.2f\n", i, SNRdB[i]);
+
+        num_packets_received[i] = 0;
+        num_valid_headers_received[i] = 0;
+        num_valid_packets_received[i] = 0;
+        num_bytes_received[i] = 0;
+    }
+
+    unsigned int num_blocks = (unsigned int)((2.0f*bandwidth*num_seconds)/(512));
+
+    // create usrp_io object and set properties
+    usrp_io * uio = new usrp_io();
+    uio->set_rx_freq(USRP_CHANNEL, frequency);
+    uio->set_rx_samplerate(2.0f*bandwidth);
+    uio->enable_auto_tx(USRP_CHANNEL);
+
+    // retrieve rx port
+    gport port_rx = uio->get_rx_port(USRP_CHANNEL);
+
+    SNRdB_av = 0.0f;
+
+    // framing
+    packetizer p = packetizer_create(0,FEC_NONE,FEC_NONE);
+    framedata fd;
+    fd.header = NULL;
+    fd.payload = NULL;
+    fd.p = p;
+    // set properties to default
+    framesyncprops_s props;
+    framesyncprops_init_default(&props);
+    props.squelch_threshold = -37.0f;
+    props.squelch_enabled = 0;
+#if 0
+    props.agc_bw0 = 1e-3f;
+    props.agc_bw1 = 1e-5f;
+    props.agc_gmin = 1e-3f;
+    props.agc_gmax = 1e4f;
+    props.pll_bw0 = 1e-3f;
+    props.pll_bw1 = 3e-5f;
+#endif
+    flexframesync fs = flexframesync_create(&props,callback,(void*)&fd);
+
+    std::complex<float> data_rx[512];
+ 
+    // start usrp data transfer
+    uio->start_rx(USRP_CHANNEL);
+    printf("usrp data transfer started\n");
+ 
+    struct rusage start;
+    struct rusage finish;
+    getrusage(RUSAGE_SELF, &start);
+    // read samples from buffer, run through frame synchronizer
+    for (i=0; i<num_blocks; i++) {
+        // grab data from port
+        gport_consume(port_rx, (void*)data_rx, 512);
+
+        // run through frame synchronizer
+        flexframesync_execute(fs, data_rx, 512);
+    }
+    getrusage(RUSAGE_SELF, &finish);
+
+    // stop usrp data transfer
+    uio->stop_rx(USRP_CHANNEL);
+    printf("usrp data transfer complete\n");
+
+    // clean up allocated objects
+    flexframesync_destroy(fs);
+    packetizer_destroy(p);
+    delete uio;
+
+#if 0
+    // print results
+    double extime = calculate_execution_time(start,finish);
+    float data_rate = 8.0f * (float)(num_bytes_received) / num_seconds;
+    float percent_headers_valid = (num_packets_received == 0) ?
+                          0.0f :
+                          100.0f * (float)num_valid_headers_received / (float)num_packets_received;
+    float percent_packets_valid = (num_packets_received == 0) ?
+                          0.0f :
+                          100.0f * (float)num_valid_packets_received / (float)num_packets_received;
+    if (num_packets_received > 0)
+        SNRdB_av /= num_packets_received;
+    float PER = 1.0f - 0.01f*percent_packets_valid;
+    float spectral_efficiency = data_rate / bandwidth;
+    printf("    packets received    : %6u\n", num_packets_received);
+    printf("    valid headers       : %6u (%6.2f%%)\n", num_valid_headers_received,percent_headers_valid);
+    printf("    valid packets       : %6u (%6.2f%%)\n", num_valid_packets_received,percent_packets_valid);
+    printf("    packet error rate   : %16.8e\n", PER);
+    printf("    average SNR [dB]    : %8.4f\n", SNRdB_av);
+    printf("    bytes_received      : %6u\n", num_bytes_received);
+    printf("    data rate           : %12.8f kbps\n", data_rate*1e-3f);
+    printf("    spectral efficiency : %12.8f b/s/Hz\n", spectral_efficiency);
+    printf("    execution time      : %12.8f s\n", extime);
+    printf("    %% cpu               : %12.8f\n", 100.0f*extime / num_seconds);
+
+    printf("    # rx   # ok      %% ok        # data     %% data       PER          SNR      sp. eff.\n");
+    printf("    %-6u %-6u  %12.4e  %-6u   %12.4e %12.4e  %8.4f  %6.4f\n",
+            num_packets_received,
+            num_valid_headers_received,
+            percent_headers_valid * 0.01f,
+            num_valid_packets_received,
+            percent_packets_valid * 0.01f,
+            PER,
+            SNRdB_av,
+            spectral_efficiency);
+#else
+    printf("    SNR [dB]    detected    headers     payloads    rate [kbps]\n");
+    for (i=0; i<num_steps; i++) {
+        printf("    %6.2f      %-6u      %-6u      %-6u    %8.3f\n",
+               SNRdB[i],
+               num_packets_received[i],
+               num_valid_headers_received[i],
+               num_valid_packets_received[i],
+               num_bytes_received[i] * 8.0f * 1e-3f);
+    }
+#endif
+    // save results to output file
+    FILE * fid = fopen(filename,"w");
+    if (!fid) {
+        fprintf(stderr,"error: could not open '%s' for writing\n", filename);
+        exit(1);
+    }
+    fprintf(fid,"# framestats data\n");
+    fprintf(fid,"#   SNR [dB]    detected    headers     payloads    rate [kbps]\n");
+    for (i=0; i<num_steps; i++) {
+        fprintf(fid,"    %6.2f      %-6u      %-6u      %-6u    %12.4e\n",
+                SNRdB[i],
+                num_packets_received[i],
+                num_valid_headers_received[i],
+                num_valid_packets_received[i],
+                num_bytes_received[i] * 8.0f * 1e-3f);
+    }
+
+    fclose(fid);
+    printf("results written to '%s'\n", filename);
+
+    // free memory arrays
+    free(SNRdB);
+    free(num_packets_received);
+    free(num_valid_headers_received);
+    free(num_valid_packets_received);
+    free(num_bytes_received);
+
+    return 0;
+}
+
---
+++ src/framestats_tx.cc
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2010 Joseph Gaeddert
+ * Copyright (c) 2010 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// framestats_tx.cc
+//
+// frame statistics transmitter
+//
+
+#include <iostream>
+#include <complex>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <liquid/liquid.h>
+
+#include "usrp_io.h"
+ 
+#define USRP_CHANNEL        (0)
+
+void usage() {
+    printf("flexframe_tx:\n");
+    printf("  f     :   center frequency [Hz]\n");
+    printf("  b     :   bandwidth [Hz]\n");
+    printf("  g     :   transmit power gain [dB] (default -3dB)\n");
+    printf("  t     :   run time [seconds]\n");
+    printf("  n     :   payload length (bytes)\n");
+    printf("  m     :   mod. scheme: <psk>, dpsk, ask, qam, apsk...\n");
+    printf("  p     :   mod. depth: <1>,2,...8\n");
+    printf("  s     :   packet spacing <0>\n");
+    printf("  r     :   ramp up/dn length <64>\n");
+    printf("  c     :   fec coding scheme (inner)\n");
+    printf("  k     :   fec coding scheme (outer)\n");
+    // print all available FEC schemes
+    unsigned int i;
+    for (i=0; i<LIQUID_NUM_FEC_SCHEMES; i++)
+        printf("              %s\n", fec_scheme_str[i]);
+    printf("  q     :   quiet\n");
+    printf("  v     :   verbose\n");
+    printf("  u,h   :   usage/help\n");
+}
+
+int main (int argc, char **argv)
+{
+    bool verbose = true;
+
+    float min_bandwidth = (32e6 / 512.0);
+    float max_bandwidth = (32e6 /   4.0);
+
+    float frequency = 462.0e6;
+    float bandwidth = min_bandwidth;
+    float num_seconds = 5.0f;
+    float txgain_dB = -3.0f;
+    float gmin_dB   = -25.0f;
+    float gmax_dB   =   0.0f;
+
+    unsigned int packet_spacing=0;
+    unsigned int payload_len=200;
+    fec_scheme fec0 = FEC_NONE;
+    fec_scheme fec1 = FEC_HAMMING74;
+    modulation_scheme mod_scheme = MOD_QAM;
+    unsigned int mod_depth = 2;
+    unsigned int ramp_len = 64;
+
+    //
+    int d;
+    while ((d = getopt(argc,argv,"f:b:g:t:n:m:p:s:r:c:k:qvuh")) != EOF) {
+        switch (d) {
+        case 'f':   frequency = atof(optarg);       break;
+        case 'b':   bandwidth = atof(optarg);       break;
+        case 'g':   txgain_dB = atof(optarg);       break;
+        case 't':   num_seconds = atof(optarg);     break;
+        case 'n':   payload_len = atoi(optarg);     break;
+        case 'm':
+            mod_scheme = liquid_getopt_str2mod(optarg);
+            if (mod_scheme == MOD_UNKNOWN) {
+                printf("error: unknown/unsupported mod. scheme: %s\n", optarg);
+                mod_scheme = MOD_UNKNOWN;
+            }
+            break;
+        case 'p':   mod_depth = atoi(optarg);       break;
+        case 's':   packet_spacing = atoi(optarg);  break;
+        case 'r':   ramp_len = atoi(optarg);        break;
+        case 'c':   fec0 = liquid_getopt_str2fec(optarg);         break;
+        case 'k':   fec1 = liquid_getopt_str2fec(optarg);         break;
+        case 'q':   verbose = false;                break;
+        case 'v':   verbose = true;                 break;
+        case 'u':
+        case 'h':
+        default:
+            usage();
+            return 0;
+        }
+    }
+
+    if (bandwidth > max_bandwidth) {
+        printf("error: maximum bandwidth exceeded (%8.4f MHz)\n", max_bandwidth*1e-6);
+        return 0;
+    } else if (bandwidth < min_bandwidth) {
+        printf("error: minimum bandwidth exceeded (%8.4f kHz)\n", min_bandwidth*1e-3);
+        return 0;
+    } else if (payload_len > (1<<16)) {
+        printf("error: maximum payload length exceeded: %u > %u\n", payload_len, 1<<16);
+        return 0;
+    } else if (fec0 == FEC_UNKNOWN || fec1 == FEC_UNKNOWN) {
+        usage();
+        return 0;
+    } else if (mod_scheme == MOD_UNKNOWN) {
+        usage();
+        return 0;
+    }
+
+    printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
+    printf("bandwidth   :   %12.8f [kHz]\n", bandwidth*1e-3f);
+    printf("tx gain     :   %12.8f [dB]\n", txgain_dB);
+    printf("verbosity   :   %s\n", (verbose?"enabled":"disabled"));
+
+    // create usrp_io object and set properties
+    usrp_io * uio = new usrp_io();
+    uio->set_tx_freq(USRP_CHANNEL, frequency);
+    uio->set_tx_samplerate(2.0f*bandwidth);
+    uio->enable_auto_tx(USRP_CHANNEL);
+
+    // retrieve tx port
+    gport port_tx = uio->get_tx_port(USRP_CHANNEL);
+
+    // packetizer
+    packetizer p = packetizer_create(payload_len,fec0,fec1);
+    unsigned int packet_len = packetizer_get_packet_length(payload_len,fec0,fec1);
+    packetizer_print(p);
+
+    // create flexframegen object
+    flexframegenprops_s fgprops;
+    fgprops.rampup_len = ramp_len;
+    fgprops.phasing_len = 64;
+    fgprops.payload_len = packet_len;
+    fgprops.mod_scheme = mod_scheme;
+    fgprops.mod_bps = mod_depth;
+    fgprops.rampdn_len = ramp_len;
+    flexframegen fg = flexframegen_create(&fgprops);
+    flexframegen_print(fg);
+
+
+    // framing buffers
+    unsigned int frame_len = flexframegen_getframelen(fg);
+    std::complex<float> frame[frame_len];
+    std::complex<float> mfbuffer[2*frame_len];
+
+    printf("frame length        :   %u\n", frame_len);
+
+    unsigned int num_blocks = (unsigned int)((4.0f*bandwidth*num_seconds)/(4*frame_len));
+
+    // create pulse-shaping interpolator
+    unsigned int m=3;
+    float beta=0.7f;
+    interp_crcf mfinterp = interp_crcf_create_rrc(2,m,beta,0);
+
+    // data buffers
+    unsigned char header[9];
+    unsigned char payload[payload_len];
+    unsigned char packet[packet_len];
+
+    // start usrp data transfer
+    uio->start_tx(USRP_CHANNEL);
+
+    // transmitter gain (linear)
+    float g = powf(10.0f, txgain_dB/10.0f);
+ 
+    unsigned int i, j, pid=0;
+    // start transmitter
+    for (i=0; i<num_blocks; i++) {
+        // generate the frame / transmit silence
+        if ((i%(packet_spacing+1))==0) {
+            // generate random tx gain
+            txgain_dB = gmin_dB + randf() * (gmax_dB - gmin_dB);
+            g = powf(10.0f, txgain_dB/10.0f);
+
+            // generate random data
+            // TODO : encode using forward error-correction codec
+            for (j=0; j<payload_len; j++)
+                payload[j] = rand() % 256;
+            // assemble packet
+            packetizer_encode(p,payload,packet);
+            // write header
+            header[0] = (pid >> 8) & 0xff;
+            header[1] = (pid     ) & 0xff;
+            header[2] = (payload_len >> 8) & 0xff;
+            header[3] = (payload_len     ) & 0xff;
+            header[4] = (unsigned char)(fec0);
+            header[5] = (unsigned char)(fec1);
+            if (verbose)
+                printf("packet id: %6u\n", pid);
+                //printf("packet id: %6u, packet len : %6u\n", pid, packet_len);
+            /*
+            for (j=0; j<payload_len; j++)
+                printf("%.2x ",payload[j]);
+            printf("\n");
+            for (j=0; j<packet_len; j++)
+                printf("%.2x ",packet[j]);
+            printf("\n");
+            */
+            pid = (pid+1) & 0xffff;
+
+            flexframegen_execute(fg, header, packet, frame);
+
+            // interpolate using matched filter
+            for (j=0; j<frame_len; j++) {
+                frame[j] *= g;
+                std::complex<float> x = j<frame_len ? frame[j] : 0.0f;
+                interp_crcf_execute(mfinterp, x, &mfbuffer[2*j]);
+            }
+
+        } else {
+            // flush interpolator with zeros
+            for (j=0; j<frame_len; j++)
+                interp_crcf_execute(mfinterp, 0.0f, &mfbuffer[2*j]);
+        }
+
+        // send data to usrp via port
+        gport_produce(port_tx,(void*)mfbuffer,2*frame_len);
+ 
+    }
+ 
+ 
+    uio->stop_tx(USRP_CHANNEL);  // Stop data transfer
+
+    // clean it up
+    packetizer_destroy(p);
+    flexframegen_destroy(fg);
+    interp_crcf_destroy(mfinterp);
+    delete uio;
+    return 0;
+}
+
