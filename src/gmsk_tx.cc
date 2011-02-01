794eb7f275f6041099f57afbf2c8205c88931164
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-01 18:00:11 -0500

    gmsk_tx: adding bpacket assembly

--- src/gmsk_tx.cc
+++ src/gmsk_tx.cc
@@ -37,7 +37,7 @@ void usage() {
     printf("  s     :   symbol rate [Hz], default 62.5 kHz\n");
     printf("  g     :   transmit power gain [dB], default -3dB\n");
     printf("  B     :   bandwidth factor, default: 0.3\n");
-    printf("  t     :   run time [seconds]\n");
+    printf("  n     :   num packets [1024]\n");
 }
 
 int main (int argc, char **argv)
@@ -47,14 +47,20 @@ int main (int argc, char **argv)
 
     float frequency = 462.0e6;
     float symbol_rate = min_symbol_rate;
-    float num_seconds = 5.0f;
     float txgain_dB = -3.0f;
 
     float BT = 0.3f;
+    unsigned int num_packets = 1024;
+
+    // packetizer properties
+    unsigned int packet_len_dec = 64;   // original data message length
+    crc_scheme check = CRC_32;          // data integrity check
+    fec_scheme fec0 = FEC_HAMMING128;   // inner code
+    fec_scheme fec1 = FEC_NONE;         // outer code
 
     //
     int d;
-    while ((d = getopt(argc,argv,"uhf:s:g:B:t:")) != EOF) {
+    while ((d = getopt(argc,argv,"uhf:s:g:B:n:")) != EOF) {
         switch (d) {
         case 'u':
         case 'h':   usage();                        return 0;
@@ -62,7 +68,7 @@ int main (int argc, char **argv)
         case 's':   symbol_rate = atof(optarg);     break;
         case 'g':   txgain_dB = atof(optarg);       break;
         case 'B':   BT = atof(optarg);              break;
-        case 't':   num_seconds = atof(optarg);     break;
+        case 'n':   num_packets = atof(optarg);     break;
         default:
             fprintf(stderr,"error: unknown/invalid option\n");
             return 0;
@@ -78,14 +84,11 @@ int main (int argc, char **argv)
     }
 
     printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
-    printf("symbol_rate   :   %12.8f [kHz]\n", symbol_rate*1e-3f);
+    printf("symbol rate :   %12.8f [kHz]\n", symbol_rate*1e-3f);
     printf("tx gain     :   %12.8f [dB]\n", txgain_dB);
 
-    // 
     unsigned int k=2;   // filter samples/symbol
     unsigned int m=3;   // filter delay
-    unsigned int frame_len = 512;
-    unsigned int num_blocks = (unsigned int)((4.0f*symbol_rate*num_seconds)/(k*frame_len));
 
     // create usrp_io object and set properties
     usrp_io * uio = new usrp_io();
@@ -96,29 +99,54 @@ int main (int argc, char **argv)
     // retrieve tx port
     gport port_tx = uio->get_tx_port(USRP_CHANNEL);
 
+    // create packet generator
+    bpacketgen pg = bpacketgen_create(0, packet_len_dec, check, fec0, fec1);
+    bpacketgen_print(pg);
+    unsigned int packet_len_enc = bpacketgen_get_packet_len(pg);
 
+    // data arrays
+    unsigned char packet_dec[packet_len_dec];
+    unsigned char packet_enc[packet_len_enc];
+    std::complex<float> buffer[packet_len_enc*8*k];
+    
     // transmitter gain (linear)
     float g = powf(10.0f, txgain_dB/10.0f);
 
     // create GMSK modem
     gmskmod mod = gmskmod_create(k,m,BT);
 
-    std::complex<float> buffer[frame_len*k];
  
-    unsigned int i, j;
+    unsigned int i;
+    unsigned int j;
+    unsigned int n;
     // start transmitter
     uio->start_tx(USRP_CHANNEL);
-    for (i=0; i<num_blocks; i++) {
-        // modulate random symbols
-        for (j=0; j<frame_len; j++)
-            gmskmod_modulate(mod, rand()%2, &buffer[k*j]);
+    for (n=0; n<num_packets; n++) {
+
+        // generate random payload
+        for (i=0; i<packet_len_dec; i++)
+            packet_dec[i] = rand() & 0xff;
+
+        // encode packet
+        bpacketgen_encode(pg, packet_dec, packet_enc);
+
+        // modulate data symbols
+        for (i=0; i<packet_len_enc; i++) {
+            unsigned int j;
+            for (j=0; j<8; j++) {
+                unsigned int sym = (packet_enc[i] >> (8-j-1)) & 1;
+                gmskmod_modulate(mod, sym, &buffer[(8*i+j)*k]);
+            }
+        }
         
+#if 0
         // apply gain
         for (j=0; j<k*frame_len; j++)
             buffer[j] *= g;
+#endif
 
         // send data to usrp via port
-        gport_produce(port_tx,(void*)buffer,k*frame_len);
+        gport_produce(port_tx,(void*)buffer,8*k*packet_len_enc);
  
     }
  
@@ -126,6 +154,7 @@ int main (int argc, char **argv)
 
     // delete objects
     gmskmod_destroy(mod);
+    bpacketgen_destroy(pg);
     delete uio;
 
     return 0;
