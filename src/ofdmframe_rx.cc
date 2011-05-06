079b877a4a1776991d2a9b010bd782f4ecbb6267
Joseph Gaeddert <jgaedder@vt.edu>
2011-05-06 10:28:37 -0400

    ofdmframe_rx: fixing min/max bandwidth range

--- src/ofdmframe_rx.cc
+++ src/ofdmframe_rx.cc
@@ -122,8 +122,8 @@ int main (int argc, char **argv)
     // command-line options
     verbose = false;
 
-    double min_bandwidth = (32e6 / 256.0);
-    double max_bandwidth = (32e6 /   2.0);
+    double min_bandwidth = 0.25*(64e6 / 512.0);
+    double max_bandwidth = 0.25*(64e6 /   4.0);
 
     double frequency = 462.0e6;
     double bandwidth = min_bandwidth;
