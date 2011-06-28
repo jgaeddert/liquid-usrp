fea920c4c6ed0907009a156c365710c591b3aa1b
Joseph Gaeddert <jgaedder@vt.edu>
2011-06-28 10:05:33 -0400

    ofdm transmitter: moving to ofdmflexframe

--- src/ofdmflexframe_tx.cc
+++ src/ofdmflexframe_tx.cc
@@ -29,7 +29,7 @@
 #include <uhd/usrp/single_usrp.hpp>
 
 void usage() {
-    printf("ofdmframe_tx -- transmit OFDM packets\n");
+    printf("ofdmflexframe_tx -- transmit OFDM packets\n");
     printf("  u,h   :   usage/help\n");
     printf("  q/v   :   quiet/verbose\n");
     printf("  f     :   center frequency [Hz]\n");
@@ -60,12 +60,11 @@ int main (int argc, char **argv)
 
     unsigned int M = 64;                // number of subcarriers
     unsigned int cp_len = 16;           // cyclic prefix length
-    unsigned int num_symbols_S0 = 2;    // number of S0 symbols
-    unsigned int num_symbols_S1 = 1;    // number of S1 symbols
+    unsigned int num_symbols_S0 = 3;    // number of S0 symbols
 
     modulation_scheme ms = LIQUID_MODEM_QAM;     // modulation scheme
     unsigned int bps = 2;               // modulation depth
-    unsigned int packet_len_dec = 50;   // original data message length
+    unsigned int payload_len = 120;     // original data message length
     crc_scheme check = LIQUID_CRC_32;          // data validity check
     fec_scheme fec0 = LIQUID_FEC_NONE;         // fec (inner)
     fec_scheme fec1 = LIQUID_FEC_HAMMING128;   // fec (outer)
@@ -187,65 +186,30 @@ int main (int argc, char **argv)
     unsigned int M_data=0;
     ofdmframe_validate_sctype(p,M, &M_null, &M_pilot, &M_data);
 
-    // number of ofdm data symbols in frame (excluding preamble)
-    unsigned int num_symbols_data = (2800/M_data) + 1;
-
-    // total number of modulated symbols in frame
-    unsigned int num_mod_symbols = num_symbols_data * M_data;
-    
-    // create packet generator
-    bpacketgen pg = bpacketgen_create(0, packet_len_dec, check, fec0, fec1);
-    bpacketgen_print(pg);
-    unsigned int packet_len_enc = bpacketgen_get_packet_len(pg);
-
     // data arrays
-    unsigned char packet_dec[packet_len_dec];
-    unsigned char packet_enc[packet_len_enc];
+    unsigned char header[8];
+    unsigned char payload[payload_len];
     
-    // 
-    div_t dsyms = div(8*packet_len_enc,bps);
-    unsigned int num_packet_symbols = dsyms.quot + (dsyms.rem ? 1 : 0);
-    unsigned int packets_per_frame = num_mod_symbols / num_packet_symbols;
-
-    printf("ofdmframe_tx properties\n");
-    printf("    null            :   %u\n", M_null);
-    printf("    plot            :   %u\n", M_pilot);
-    printf("    data            :   %u\n", M_data);
-    printf("    ofdm syms/frame :   %u\n", num_symbols_data);
-    printf("    mod. syms/frame :   %u\n", num_mod_symbols);
-    printf("    packet len dec  :   %u bytes\n", packet_len_dec);
-    printf("    packet len enc  :   %u bytes\n", packet_len_enc);
-    printf("    mod. syms/packet:   %u\n", num_packet_symbols);
-    printf("    packets/frame   :   %u\n", packets_per_frame);
-
     // create frame generator
-    ofdmframegen fg = ofdmframegen_create(M, cp_len, p);
-    ofdmframegen_print(fg);
+    ofdmflexframegenprops_s fgprops;
+    ofdmflexframegenprops_init_default(&fgprops);
+    fgprops.num_symbols_S0  = num_symbols_S0;
+    fgprops.payload_len     = payload_len;
+    fgprops.check           = check;
+    fgprops.fec0            = fec0;
+    fgprops.fec1            = fec1;
+    fgprops.mod_scheme      = ms;
+    fgprops.mod_bps         = bps;
+    ofdmflexframegen fg = ofdmflexframegen_create(M, cp_len, p, &fgprops);
+    ofdmflexframegen_print(fg);
 
     // arrays
-    std::complex<float> modsyms[num_packet_symbols];
-    std::complex<float> X[M];           // channelized symbols
-    std::complex<float> S0[M];          // PLCP sequence (short)
-    std::complex<float> S1[M];          // PLCP sequence (long)
-    std::complex<float> y[M+cp_len];    // output time series
-
-    // full frame
-    unsigned int frame_len = num_symbols_S0 * M +
-                             num_symbols_S1 * M + cp_len +
-                             num_symbols_data*(M+cp_len);
-    std::complex<float> frame[frame_len];
-    std::complex<float> buffer_interp[2*frame_len];
-    std::complex<float> buffer_resamp[3*frame_len];
-
-    // initialize sequence arrays
-    ofdmframegen_write_S0(fg, S0);
-    ofdmframegen_write_S1(fg, S1);
-
-    // create modem
-    modem mod = modem_create(ms,bps);
+    std::complex<float> buffer[M+cp_len];    // output time series
+    std::complex<float> buffer_interp[2*(M+cp_len)];
+    std::complex<float> buffer_resamp[3*(M+cp_len)];
 
     // set up the metadta flags
-    std::vector<std::complex<float> > buff(M+cp_len);
+    std::vector<std::complex<float> > buff(3*(M+cp_len));
     uhd::tx_metadata_t md;
     md.start_of_burst = false;  // never SOB when continuous
     md.end_of_burst   = false;  // 
@@ -254,130 +218,56 @@ int main (int argc, char **argv)
     unsigned int num_frames = -1;
 
     unsigned int j;
-    unsigned int num_samples=0;
     for (i=0; i<num_frames; i++) {
 
-        //
-        // preamble
-        //
-        num_samples=0;
+        // reset frame generator (resets pilot generator, etc.)
+        ofdmflexframegen_reset(fg);
 
-        // write short sequence(s)
-        for (j=0; j<num_symbols_S0; j++) {
-            memmove(&frame[num_samples], S0, M*sizeof(std::complex<float>));
-            num_samples += M;
-        }
+        // initialize header/payload and assemble frame
+        for (j=0; j<8; j++)
+            header[i] = rand() & 0xff;
+        for (j=0; j<payload_len; j++)
+            payload[i] = rand() & 0xff;
 
-        // write long sequence extension
-        memmove(&frame[num_samples], &S1[M-cp_len], cp_len*sizeof(std::complex<float>));
-        num_samples += cp_len;
+        // assemble frame
+        ofdmflexframegen_assemble(fg, header, payload);
 
-        // write long sequence(s)
-        for (j=0; j<num_symbols_S1; j++) {
-            memmove(&frame[num_samples], S1, M*sizeof(std::complex<float>));
-            num_samples += M;
-        }
+        // generate frame
+        int last_symbol=0;
+        unsigned int num_samples;
+        while (!last_symbol) {
+            // generate symbol
+            last_symbol = ofdmflexframegen_writesymbol(fg, buffer, &num_samples);
 
-        //
-        // payload
-        //
-
-        // payload modem symbol counter
-        unsigned int k=0;           // modem symbol counter
-        unsigned int n;
-        unsigned int num_packets=0; // packet counter
-
-        // generate ofdm payload symbols
-        for (n=0; n<num_symbols_data; n++) {
-
-            // load data onto subcarriers
-            for (j=0; j<M; j++) {
-
-                // ignore non-data subcarriers
-                if (p[j] != OFDMFRAME_SCTYPE_DATA)
-                    continue;
-
-                // generate payload
-                if (k==0 && num_packets < packets_per_frame) {
-                    unsigned int ii;
-                    for (ii=0; ii<packet_len_dec; ii++)
-                        packet_dec[ii] = rand() & 0xff;
-
-                    // encode packet
-                    bpacketgen_encode(pg, packet_dec, packet_enc);
-
-                    // generate data symbols
-                    for (ii=0; ii<num_packet_symbols; ii++) {
-                        unsigned char sym=0;
-                        liquid_unpack_array(packet_enc,
-                                            packet_len_enc,
-                                            ii*bps,     // bit index
-                                            bps,        // symbol size
-                                            &sym);      // output symbol
-
-                        // modulate symbol
-                        modem_modulate(mod, sym, &modsyms[ii]);
-                    }
-                    num_packets++;
-                } else if (k==0 && num_packets >= packets_per_frame) {
-                    //printf("random payload\n");
-                    unsigned int ii;
-                    for (ii=0; ii<num_packet_symbols; ii++) {
-                        unsigned char sym = modem_gen_rand_sym(mod);
-                        modem_modulate(mod, sym, &modsyms[ii]);
-                    }
-                }
-
-                // store symbol to ofdm input subcarrier array
-                X[j] = modsyms[k];
-
-                // update counter
-                k = (k+1) % num_packet_symbols;
+            // interpolate by 2
+            for (j=0; j<num_samples; j++)
+                resamp2_crcf_interp_execute(interp, buffer[j], &buffer_interp[2*j]);
+            
+            // resample
+            unsigned int nw;
+            unsigned int n=0;
+            for (j=0; j<2*num_samples; j++) {
+                resamp_crcf_execute(resamp, buffer_interp[j], &buffer_resamp[n], &nw);
+                n += nw;
             }
 
-            // generate ofdm payload symbol
-            ofdmframegen_writesymbol(fg, X, y);
+            buff.resize(n);
+            for (j=0; j<n; j++)
+                buff[j] = g*buffer_resamp[j];
 
-            //gport_produce(port_tx, (void*)y, M+cp_len);
-            memmove(&frame[num_samples], y, (M+cp_len)*sizeof(std::complex<float>));
-            num_samples += M+cp_len;
+            //send the entire contents of the buffer
+            usrp->get_device()->send(
+                &buff.front(), buff.size(), md,
+                uhd::io_type_t::COMPLEX_FLOAT32,
+                uhd::device::SEND_MODE_FULL_BUFF
+            );
         }
 
+#if 0
         // pad remaining samples with zeros
         for (j=num_samples; j<frame_len; j++)
             frame[j] = 0.0f;
-
-        // interpolate by 2
-        for (j=0; j<frame_len; j++) {
-            //
-            resamp2_crcf_interp_execute(interp, frame[j], &buffer_interp[2*j]);
-        }
-        
-        // resample
-        unsigned int nw;
-        n=0;
-        for (j=0; j<2*frame_len; j++) {
-            resamp_crcf_execute(resamp, buffer_interp[j], &buffer_resamp[n], &nw);
-            n += nw;
-        }
-
-        buff.resize(n);
-        for (j=0; j<n; j++) {
-            buff[j] = g*buffer_resamp[j];
-        }
-
-        //send the entire contents of the buffer
-        usrp->get_device()->send(
-            &buff.front(), buff.size(), md,
-            uhd::io_type_t::COMPLEX_FLOAT32,
-            uhd::device::SEND_MODE_FULL_BUFF
-        );
-
-        // reset frame generator (resets pilot generator, etc.)
-        ofdmframegen_reset(fg);
-
-        if (verbose)
-            printf("frame transmitted %2u / %2u packets\n", num_packets, packets_per_frame);
+#endif
     }
  
     // send a mini EOB packet
@@ -392,11 +282,10 @@ int main (int argc, char **argv)
     printf("usrp data transfer complete\n");
 
     // clean it up
-    ofdmframegen_destroy(fg);
-    modem_destroy(mod);
-    bpacketgen_destroy(pg);
+    ofdmflexframegen_destroy(fg);
     resamp2_crcf_destroy(interp);
     resamp_crcf_destroy(resamp);
+
     return 0;
 }
 
