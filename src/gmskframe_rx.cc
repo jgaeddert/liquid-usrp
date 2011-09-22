598eaed297fe501d4edaa022e97a479f41f7068a
Joseph Gaeddert <jgaedder@vt.edu>
2011-09-22 11:28:01 -0400

    adding gmskframe transmitter/receiver applications

---
+++ src/gmskframe_rx.cc
@@ -0,0 +1,295 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <complex>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <liquid/liquid.h>
+
+#include <uhd/usrp/single_usrp.hpp>
+ 
+static bool verbose;
+static unsigned int num_packets_received;
+static unsigned int num_valid_headers_received;
+static unsigned int num_valid_packets_received;
+static unsigned int num_bytes_received;
+
+static float SNRdB_av;
+
+static int callback(unsigned char *  _header,
+                    int              _header_valid,
+                    unsigned char *  _payload,
+                    unsigned int     _payload_len,
+                    int              _payload_valid,
+                    framesyncstats_s _stats,
+                    void *           _userdata)
+{
+    num_packets_received++;
+    if (verbose) {
+        printf("********* callback invoked, ");
+        printf("evm=%5.1fdB, ", _stats.evm);
+        printf("rssi=%5.1fdB, ", _stats.rssi);
+    }
+
+    // make better estimate of SNR
+    float noise_floor = -38.0f; // noise floor estimate
+    float SNRdB = _stats.rssi - noise_floor;
+    SNRdB_av += SNRdB;
+
+    if ( !_header_valid ) {
+        if (verbose) printf("header crc : FAIL\n");
+        return 0;
+    }
+    num_valid_headers_received++;
+    unsigned int packet_id = (_header[0] << 8 | _header[1]);
+    if (verbose) printf("packet id: %6u\n", packet_id);
+
+    if ( !_payload_valid ) {
+        if (verbose) printf("payload crc : FAIL\n");
+        return 0;
+    }
+
+    num_valid_packets_received++;
+    num_bytes_received += _payload_len;
+
+    return 0;
+}
+
+void usage() {
+    printf("gmskframe_tx:\n");
+    printf("  f     :   center frequency [Hz]\n");
+    printf("  b     :   bandwidth [Hz]\n");
+    printf("  t     :   run time [seconds]\n");
+    printf("  G     :   uhd rx gain [dB] (default: 20dB)\n");
+    printf("  S     :   squelch threshold [dB] (default: -37dB)\n");
+    printf("  q     :   quiet\n");
+    printf("  v     :   verbose\n");
+    printf("  u,h   :   usage/help\n");
+}
+
+int main (int argc, char **argv)
+{
+    // command-line options
+    verbose = true;
+    unsigned long int ADC_RATE = 64e6;
+
+    float min_bandwidth = 0.25f*(ADC_RATE / 256.0);
+    float max_bandwidth = 0.25f*(ADC_RATE /   4.0);
+
+    float frequency = 462.0e6;
+    float bandwidth = 100e3;
+    float num_seconds = 5.0f;
+    double uhd_rxgain = 20.0;
+    float squelch_threshold = -37.0f;
+
+    //
+    int d;
+    while ((d = getopt(argc,argv,"f:b:t:G:S:qvuh")) != EOF) {
+        switch (d) {
+        case 'f':   frequency = atof(optarg);       break;
+        case 'b':   bandwidth = atof(optarg);       break;
+        case 't':   num_seconds = atof(optarg);     break;
+        case 'G':   uhd_rxgain = atof(optarg);      break;
+        case 'S':   squelch_threshold = atof(optarg);   break;
+        case 'q':   verbose = false;                break;
+        case 'v':   verbose = true;                 break;
+        case 'u':
+        case 'h':
+        default:
+            usage();
+            return 0;
+        }
+    }
+
+    if (bandwidth > max_bandwidth) {
+        printf("error: maximum bandwidth exceeded (%8.4f MHz)\n", max_bandwidth*1e-6);
+        return 0;
+    } else if (bandwidth < min_bandwidth) {
+        printf("error: minimum bandwidth exceeded (%8.4f kHz)\n", min_bandwidth*1e-3);
+        return 0;
+    }
+
+    printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
+    printf("bandwidth   :   %12.8f [kHz]\n", bandwidth*1e-3f);
+    printf("verbosity   :   %s\n", (verbose?"enabled":"disabled"));
+
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+
+    stream_cmd.stream_now = true;
+
+    uhd::device_addr_t dev_addr;
+    uhd::usrp::single_usrp::sptr usrp = uhd::usrp::single_usrp::make(dev_addr);
+
+    // set properties
+    float rx_rate = 4.0f*bandwidth;
+#if 0
+    usrp->set_rx_rate(rx_rate);
+#else
+    // NOTE : the sample rate computation MUST be in double precision so
+    //        that the UHD can compute its decimation rate properly
+    unsigned int decim_rate = (unsigned int)(ADC_RATE / rx_rate);
+    // ensure multiple of 2
+    decim_rate = (decim_rate >> 1) << 1;
+    // compute usrp sampling rate
+    double usrp_rx_rate = ADC_RATE / decim_rate;
+    // compute arbitrary resampling rate
+    double rx_resamp_rate = rx_rate / usrp_rx_rate;
+    printf("sample rate :   %12.8f kHz = %12.8f * %8.6f (decim %u)\n",
+            rx_rate * 1e-3f,
+            usrp_rx_rate * 1e-3f,
+            rx_resamp_rate,
+            decim_rate);
+    usrp->set_rx_rate(ADC_RATE / decim_rate);
+#endif
+    usrp->set_rx_freq(frequency);
+    usrp->set_rx_gain(uhd_rxgain);
+
+    // add arbitrary resampling component
+    resamp_crcf resamp = resamp_crcf_create(rx_resamp_rate,7,0.4f,60.0f,64);
+    resamp_crcf_setrate(resamp, rx_resamp_rate);
+
+    // half-band resampler
+    resamp2_crcf decim = resamp2_crcf_create(7,0.0f,40.0f);
+
+    const size_t max_samps_per_packet = usrp->get_device()->get_max_recv_samps_per_packet();
+    unsigned int num_blocks = (unsigned int)((rx_rate*num_seconds)/(max_samps_per_packet));
+
+    //allocate recv buffer and metatdata
+    uhd::rx_metadata_t md;
+    std::vector<std::complex<float> > buff(max_samps_per_packet);
+
+    num_packets_received = 0;
+    num_valid_packets_received = 0;
+    num_valid_headers_received = 0;
+    num_bytes_received = 0;
+    SNRdB_av = 0.0f;
+
+    // create frame synchronizer
+    unsigned int k = 2;
+    unsigned int m = 4;
+    float BT = 0.5f;
+    gmskframesync fs = gmskframesync_create(k,m,BT,callback,NULL);
+
+    std::complex<float> data_rx[64];
+    std::complex<float> data_decim[32];
+    std::complex<float> data_resamp[64];
+
+    // start data transfer
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    printf("usrp data transfer started\n");
+ 
+    unsigned int i;
+    unsigned int n=0;
+    for (i=0; i<num_blocks; i++) {
+        // grab data from port
+        size_t num_rx_samps = usrp->get_device()->recv(
+            &buff.front(), buff.size(), md,
+            uhd::io_type_t::COMPLEX_FLOAT32,
+            uhd::device::RECV_MODE_ONE_PACKET
+        );
+
+        //handle the error codes
+        switch(md.error_code){
+        case uhd::rx_metadata_t::ERROR_CODE_NONE:
+        case uhd::rx_metadata_t::ERROR_CODE_OVERFLOW:
+            break;
+
+        default:
+            std::cerr << "Error code: " << md.error_code << std::endl;
+            std::cerr << "Unexpected error on recv, exit test..." << std::endl;
+            return 1;
+        }
+
+        if (not md.has_time_spec){
+            std::cerr << "Metadata missing time spec, exit test..." << std::endl;
+            return 1;
+        }
+
+        // for now copy vector "buff" to array of complex float
+        // TODO : apply bandwidth-dependent gain
+        unsigned int j;
+        unsigned int nw=0;
+        for (j=0; j<num_rx_samps; j++) {
+            // push 64 samples into buffer
+            data_rx[n++] = buff[j];
+
+            if (n==64) {
+                // reset counter
+                n=0;
+
+                // deimate to 32
+                unsigned int k;
+                for (k=0; k<32; k++)
+                    resamp2_crcf_decim_execute(decim, &data_rx[2*k], &data_decim[k]);
+
+                // apply resampler
+                for (k=0; k<32; k++) {
+                    resamp_crcf_execute(resamp, data_decim[k], &data_resamp[n], &nw);
+                    n += nw;
+                }
+
+                // push through synchronizer
+                gmskframesync_execute(fs, data_resamp, n);
+
+                // reset counter (again)
+                n = 0;
+            }
+
+        }
+
+    }
+ 
+
+    // stop data transfer
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+    printf("\n");
+    printf("usrp data transfer complete\n");
+
+    // print results
+    float data_rate = 8.0f * (float)(num_bytes_received) / num_seconds;
+    float percent_headers_valid = (num_packets_received == 0) ?
+                          0.0f :
+                          100.0f * (float)num_valid_headers_received / (float)num_packets_received;
+    float percent_packets_valid = (num_packets_received == 0) ?
+                          0.0f :
+                          100.0f * (float)num_valid_packets_received / (float)num_packets_received;
+    if (num_packets_received > 0)
+        SNRdB_av /= num_packets_received;
+    float PER = 1.0f - 0.01f*percent_packets_valid;
+    float spectral_efficiency = data_rate / bandwidth;
+    printf("    packets received    : %6u\n", num_packets_received);
+    printf("    valid headers       : %6u (%6.2f%%)\n", num_valid_headers_received,percent_headers_valid);
+    printf("    valid packets       : %6u (%6.2f%%)\n", num_valid_packets_received,percent_packets_valid);
+    printf("    packet error rate   : %16.8e\n", PER);
+    printf("    average SNR [dB]    : %8.4f\n", SNRdB_av);
+    printf("    bytes_received      : %6u\n", num_bytes_received);
+    printf("    data rate           : %12.8f kbps\n", data_rate*1e-3f);
+    printf("    spectral efficiency : %12.8f b/s/Hz\n", spectral_efficiency);
+
+    // clean it up
+    gmskframesync_destroy(fs);
+    resamp_crcf_destroy(resamp);
+    resamp2_crcf_destroy(decim);
+
+    return 0;
+}
+
---
+++ src/gmskframe_tx.cc
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <complex>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <liquid/liquid.h>
+
+#include <uhd/usrp/single_usrp.hpp>
+
+void usage() {
+    printf("gmskframe_tx:\n");
+    printf("  f     :   center frequency [Hz]\n");
+    printf("  b     :   bandwidth [Hz]\n");
+    printf("  g     :   software transmit power gain [dB] (default: -3dB)\n");
+    printf("  G     :   uhd tx gain [dB] (default: -40dB)\n");
+    printf("  t     :   run time [seconds]\n");
+    printf("  n     :   payload length (bytes)\n");
+    printf("  m     :   mod. scheme: <psk>, dpsk, ask, qam, apsk...\n");
+    printf("  p     :   mod. depth: <1>,2,...8\n");
+    printf("  s     :   packet spacing <0>\n");
+    printf("  r     :   ramp up/dn length <64>\n");
+    printf("  c     :   fec coding scheme (inner)\n");
+    printf("  k     :   fec coding scheme (outer)\n");
+    // print all available FEC schemes
+    unsigned int i;
+    for (i=0; i<LIQUID_FEC_NUM_SCHEMES; i++)
+        printf("          [%s] %s\n", fec_scheme_str[i][0], fec_scheme_str[i][1]);
+    printf("  q     :   quiet\n");
+    printf("  v     :   verbose\n");
+    printf("  u,h   :   usage/help\n");
+}
+
+int main (int argc, char **argv)
+{
+    bool verbose = true;
+
+    unsigned long int DAC_RATE = 64e6;
+    float min_bandwidth = 0.25f*(DAC_RATE / 256.0);
+    float max_bandwidth = 0.25f*(DAC_RATE /   4.0);
+
+    float frequency = 462.0e6;
+    float bandwidth = 100e3;
+    float num_seconds = 5.0f;
+    float txgain_dB = -3.0f;
+    double uhd_txgain = -40.0;
+
+    unsigned int packet_spacing=0;                      // spacing b/w frames
+    unsigned int payload_len=200;                       // payload length (bytes)
+    crc_scheme check    = LIQUID_CRC_16;                // data validity check
+    fec_scheme fec0     = LIQUID_FEC_NONE;              // inner FEC scheme
+    fec_scheme fec1     = LIQUID_FEC_HAMMING74;         // outer FEC scheme
+    modulation_scheme mod_scheme = LIQUID_MODEM_QAM;    // modulation scheme
+    unsigned int mod_depth = 2;                         // modulation depth
+    unsigned int ramp_len = 64;                         // phasing ramp up/down length
+
+    //
+    int d;
+    while ((d = getopt(argc,argv,"f:b:g:G:t:n:m:p:s:r:c:k:qvuh")) != EOF) {
+        switch (d) {
+        case 'f':   frequency = atof(optarg);       break;
+        case 'b':   bandwidth = atof(optarg);       break;
+        case 'g':   txgain_dB = atof(optarg);       break;
+        case 'G':   uhd_txgain = atof(optarg);      break;
+        case 't':   num_seconds = atof(optarg);     break;
+        case 'n':   payload_len = atoi(optarg);     break;
+        case 'm':
+            mod_scheme = liquid_getopt_str2mod(optarg);
+            break;
+        case 'p':   mod_depth = atoi(optarg);       break;
+        case 's':   packet_spacing = atoi(optarg);  break;
+        case 'r':   ramp_len = atoi(optarg);        break;
+        case 'c':   fec0 = liquid_getopt_str2fec(optarg);         break;
+        case 'k':   fec1 = liquid_getopt_str2fec(optarg);         break;
+        case 'q':   verbose = false;                break;
+        case 'v':   verbose = true;                 break;
+        case 'u':
+        case 'h':
+        default:
+            usage();
+            return 0;
+        }
+    }
+
+    if (bandwidth > max_bandwidth) {
+        fprintf(stderr,"error: maximum bandwidth exceeded (%8.4f MHz)\n", max_bandwidth*1e-6);
+        return 1;
+    } else if (bandwidth < min_bandwidth) {
+        fprintf(stderr,"error: minimum bandwidth exceeded (%8.4f kHz)\n", min_bandwidth*1e-3);
+        return 1;
+    } else if (payload_len > (1<<16)) {
+        fprintf(stderr,"error: maximum payload length exceeded: %u > %u\n", payload_len, 1<<16);
+        return 1;
+    } else if (fec0 == LIQUID_FEC_UNKNOWN || fec1 == LIQUID_FEC_UNKNOWN) {
+        fprintf(stderr,"error: unsupported FEC scheme\n");
+        return 1;
+    } else if (mod_scheme == LIQUID_MODEM_UNKNOWN) {
+        fprintf(stderr,"error: unsupported modulation scheme\n");
+        return 0;
+    }
+
+    printf("frequency   :   %12.8f [MHz]\n", frequency*1e-6f);
+    printf("bandwidth   :   %12.8f [kHz]\n", bandwidth*1e-3f);
+    printf("tx gain     :   %12.8f [dB]\n", txgain_dB);
+    printf("verbosity   :   %s\n", (verbose?"enabled":"disabled"));
+
+    uhd::device_addr_t dev_addr;
+    //dev_addr["addr0"] = "192.168.10.2";
+    //dev_addr["addr1"] = "192.168.10.3";
+    uhd::usrp::single_usrp::sptr usrp = uhd::usrp::single_usrp::make(dev_addr);
+
+    // set properties
+    double tx_rate = 4.0*bandwidth;
+#if 0
+    usrp->set_tx_rate(tx_rate);
+#else
+    // NOTE : the sample rate computation MUST be in double precision so
+    //        that the UHD can compute its interpolation rate properly
+    unsigned int interp_rate = (unsigned int)(DAC_RATE / tx_rate);
+    // ensure multiple of 4
+    interp_rate = (interp_rate >> 2) << 2;
+    interp_rate += 4; // ensure tx_resamp_rate <= 1.0
+    // compute usrp sampling rate
+    double usrp_tx_rate = DAC_RATE / (double)interp_rate;
+    //usrp_tx_rate = 262295.081967213;
+    // compute arbitrary resampling rate
+    double tx_resamp_rate = usrp_tx_rate / tx_rate;
+    printf("sample rate :   %12.8f kHz = %12.8f / %8.6f (interp %u)\n",
+            tx_rate * 1e-3f,
+            usrp_tx_rate * 1e-3f,
+            tx_resamp_rate,
+            interp_rate);
+
+    usrp->set_tx_rate(DAC_RATE / interp_rate);
+#endif
+    usrp->set_tx_freq(frequency);
+    usrp->set_tx_gain(uhd_txgain);
+    // set the IF filter bandwidth
+    //usrp->set_tx_bandwidth(2.0f*tx_rate);
+
+    // add arbitrary resampling component
+    resamp_crcf resamp = resamp_crcf_create(tx_resamp_rate,7,0.4f,60.0f,64);
+    resamp_crcf_setrate(resamp, tx_resamp_rate);
+
+    // half-band resampler
+    resamp2_crcf interp = resamp2_crcf_create(7,0.0f,40.0f);
+
+    // create gmskframegen object
+    unsigned int k = 2;
+    unsigned int m = 4;
+    float BT = 0.5f;
+    gmskframegen fg = gmskframegen_create(k,m,BT);
+    gmskframegen_print(fg);
+
+    // set up the metadta flags
+    uhd::tx_metadata_t md;
+    md.start_of_burst = false;  // never SOB when continuous
+    md.end_of_burst   = false;  // 
+    md.has_time_spec  = false;  // set to false to send immediately
+
+    // framing buffers
+    std::complex<float> buffer[k];
+    std::complex<float> buffer_interp[2*k];
+    std::complex<float> buffer_resamp[8*k]; // resampler
+    std::vector<std::complex<float> > buff(256);
+    unsigned int tx_buffer_samples = 0;
+
+    //unsigned int num_blocks = (unsigned int)((4.0f*bandwidth*num_seconds)/(4*frame_len));
+    unsigned int num_blocks = 1024;
+
+    // data buffers
+    unsigned char header[8];
+    unsigned char payload[payload_len];
+
+    // transmitter gain (linear)
+    float g = powf(10.0f, txgain_dB/10.0f);
+ 
+    unsigned int i, j, pid=0;
+    // start transmitter
+    for (i=0; i<num_blocks; i++) {
+        // generate random data
+        for (j=0; j<payload_len; j++)
+            payload[j] = rand() & 0xff;
+
+        // write header (first two bytes packet ID, remaining are random)
+        header[0] = (pid >> 8) & 0xff;
+        header[1] = (pid     ) & 0xff;
+        for (j=2; j<8; j++)
+            header[j] = rand() & 0xff;
+
+        if (verbose)
+            printf("packet id: %6u\n", pid);
+        
+        // increment packet id (modulo 2^16)
+        pid = (pid+1) & 0xffff;
+
+        // generate frame
+        //gmskframegen_execute(fg, header, payload, frame);
+        gmskframegen_assemble(fg, header, payload, payload_len, check, fec0, fec1);
+
+        //
+        //unsigned int frame_len = gmskframegen_get_frame_len(fg);
+        
+        // 
+        // generate frame
+        //
+        int frame_complete = 0;
+        while (!frame_complete) {
+            // generate k samples
+            frame_complete = gmskframegen_write_samples(fg, buffer);
+
+            // interpolate by 2
+            for (j=0; j<k; j++)
+                resamp2_crcf_interp_execute(interp, buffer[j], &buffer_interp[2*j]);
+
+            // resample
+            // resample
+            unsigned int nw;
+            unsigned int n=0;
+            for (j=0; j<2*k; j++) {
+                resamp_crcf_execute(resamp, buffer_interp[j], &buffer_resamp[n], &nw);
+                n += nw;
+            }
+
+            // push samples into buffer
+            for (j=0; j<n; j++) {
+                buff[tx_buffer_samples++] = g*buffer_resamp[j];
+
+                if (tx_buffer_samples==256) {
+                    // reset counter
+                    tx_buffer_samples=0;
+
+                    //send the entire contents of the buffer
+                    usrp->get_device()->send(
+                        &buff.front(), buff.size(), md,
+                        uhd::io_type_t::COMPLEX_FLOAT32,
+                        uhd::device::SEND_MODE_FULL_BUFF
+                    );
+                }
+            }
+        }
+
+ 
+    }
+ 
+    // send a mini EOB packet
+    md.start_of_burst = false;
+    md.end_of_burst   = true;
+    usrp->get_device()->send("", 0, md,
+        uhd::io_type_t::COMPLEX_FLOAT32,
+        uhd::device::SEND_MODE_FULL_BUFF
+    );
+
+    //finished
+    printf("usrp data transfer complete\n");
+
+ 
+    // clean it up
+    gmskframegen_destroy(fg);
+    resamp2_crcf_destroy(interp);
+    resamp_crcf_destroy(resamp);
+    return 0;
+}
+
